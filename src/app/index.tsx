d33c3f3da8e445119c47db33047d93f5cb8ed235
Sergey Konokhov <konokhovsergey@yandex.ru>
2023-11-30 11:16:59 +0300

    Добавляет алиасы в проект

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
-import Footer from '../components/footer';
-import Header from '../components/header';
-import MainContent from '../components/main-content';
-import PageLayout from '../components/page-layout';
+import Footer from '@components/footer';
+import Header from '@components/header';
+import MainContent from '@components/main-content';
+import PageLayout from '@components/page-layout';
 
 function App() {
   return (
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,4 @@
-import Container from '../container';
+import Container from '@components/container';
 import styles from './style.module.scss';
 
 function Footer() {
