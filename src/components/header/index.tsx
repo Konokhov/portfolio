dfa43a5ebc39b6d2a680fb48ec57aace836b6e99
Sergey Konokhov <konokhovsergey@yandex.ru>
2023-12-26 15:12:47 +0300

    Добавляет компонент логотипа

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,15 @@
+import Container from '@components/container';
+import Logo from '@components/logo';
 import styles from './style.module.scss';
 
 function Header() {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <Container>
+        <Logo />
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
---
+++ src/components/logo/index.tsx
@@ -0,0 +1,7 @@
+import styles from './style.module.scss';
+
+function Logo() {
+  return <h1 className={styles.logo}>Портфолио</h1>;
+}
+
+export default Logo;
