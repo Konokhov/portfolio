51cc8f2999141b0445cebcec9c3f0e85dba03e3e
Sergey Konokhov <konokhovsergey@yandex.ru>
2024-01-05 11:20:05 +0300

    Добавляет компонент Avatar на страницу

--- src/components/main-content/index.tsx
+++ src/components/main-content/index.tsx
@@ -1,7 +1,17 @@
+import Avatar from '@components/avatar';
+import Container from '@components/container';
 import styles from './style.module.scss';
 
 function MainContent() {
-  return <main className={styles.content}></main>;
+  return (
+    <main className={styles.content}>
+      <div className={styles.layout}>
+        <Container>
+          <Avatar />
+        </Container>
+      </div>
+    </main>
+  );
 }
 
 export default MainContent;
